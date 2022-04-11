e1376ec3a62bb384ea3467ed7bf45f10a0d50d00
Anton <anton.madzharovbs@gmail.com>
2022-04-11 21:51:24 +0300

    add workerId in json response for sorting

---
+++ src/main/java/com/app/workerpool/service/RestConfig.java
@@ -0,0 +1,16 @@
+package com.app.workerpool.service;
+
+import com.app.workerpool.models.Worker;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+@Component
+public class RestConfig implements RepositoryRestConfigurer {
+
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
+        config.exposeIdsFor(Worker.class);
+    }
+}
